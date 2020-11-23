a62fb57ae59a10dfff4a88da2e1e671d86430408
Rishabh Raj <rishabh053@gmail.com>
2020-11-23 21:32:18 +0530

    master

--- handler/message/text/id.js
+++ handler/message/text/id.js
@@ -23,6 +23,7 @@ Hi, ${pushname || ''}! 👋️
 Here are some of the features of this bot! ✨
 
 Sticker Maker:
+
 1. *#sticker*
 To convert an image into a sticker, send the image with the caption #sticker or reply to the image that has been sent with #sticker.
 
@@ -32,7 +33,8 @@ To change the image from the url to a sticker.
 3. *#gifsticker* _<Giphy URL>_ / *#stickergif* _<Giphy URL>_
 To turn a gif into a sticker (Giphy only)
 
-Downloader(Only Videos)
+Downloader (Only Videos)
+
 1. *#tiktok* _<post / video url>_
 Will return video tiktok.
 
