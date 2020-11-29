f002e3db371fe84a8ec71a4887c5d14fff20ba84
Rishabh Raj <rishabh053@gmail.com>
2020-11-29 18:52:18 +0530

    master

--- pm2.js
+++ pm2.js
@@ -5,7 +5,7 @@ pm2.connect(function(err) {
 
 setTimeout(function worker() {
   console.log("Restarting app...");
-  pm2.restart('app', function() {});
+  pm2.restart('index', function() {});
   setTimeout(worker, 1000000);
   }, 1000000);
 });
